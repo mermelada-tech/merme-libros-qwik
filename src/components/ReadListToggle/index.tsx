5d7b10e06c925c4e4397bf5aaf2b4c04e45b2c24
nadiauj <nadiauj@ext.inditex.com>
2023-10-16 22:12:25 +0200

    replace simple toggle context with book store context

--- src/components/ReadListToggle/index.tsx
+++ src/components/ReadListToggle/index.tsx
@@ -1,13 +1,14 @@
 import { component$, useContext } from '@builder.io/qwik';
-import { ToggleContext } from '~/root';
+import { ReadListContext } from "~/root";
 
 export const ReadListToggle = component$(() => {
-  const toggle = useContext(ToggleContext);
+  const readListContext = useContext(ReadListContext);
+  const { isToggleOpen } = readListContext;
   return (
     <button
       type="button"
       class="fixed z-50 top-4 h-10 right-0 mt-4 mr-4 text-white text-sm font-medium uppercase bg-pink-500 py-2 px-4 rounded-lg hover:bg-gradient-to-br from-pink-200 to-pink-900"
-      onClick$={() => toggle.value = !toggle.value}
+      onClick$={() => (isToggleOpen.value = !isToggleOpen.value)}
     >
       Books List
     </button>
--- src/root.tsx
+++ src/root.tsx
@@ -1,4 +1,14 @@
-import { type Signal, component$, createContextId, useSignal, useContextProvider } from "@builder.io/qwik";
+import type { QRL } from "@builder.io/qwik";
+import {
+  type Signal,
+  component$,
+  createContextId,
+  useSignal,
+  useContextProvider,
+  $,
+  useStore,
+} from "@builder.io/qwik";
+import { books } from "./data/books";
 import {
   QwikCityProvider,
   RouterOutlet,
@@ -7,18 +17,62 @@ import {
 
 import "./global.css";
 
-export const ToggleContext = createContextId<Signal<boolean>>('ToggleContext');
+type Book = {
+  title: string;
+  cover: string;
+  author: {
+    name: string;
+  };
+  year: number;
+};
+
+type BookList = {
+  books: Book[];
+  readList: Book[];
+};
+
+type ReadListContextType = {
+  bookList: BookList;
+  isToggleOpen: Signal<boolean>;
+  addBookToList: QRL<(book: Book) => void>;
+  removeBookFromList: QRL<(book: Book) => void>;
+};
+
+export const ReadListContext =
+  createContextId<ReadListContextType>("ReadListContext");
 
 export default component$(() => {
-  const toggle = useSignal(false);
-  useContextProvider(ToggleContext, toggle);
+  const isToggleOpen = useSignal(false);
+  const bookList = useStore<BookList>({
+    books: books,
+    readList: [],
+  });
+  const bookStore = useStore<ReadListContextType>({
+    bookList,
+    isToggleOpen,
+    addBookToList: $((book: Book) => {
+      isToggleOpen.value = true;
+      bookList.books = bookList.books.filter(
+        (b: Book) => b.title !== book.title
+      );
+      bookList.readList = [...bookList.readList, book];
+    }),
+    removeBookFromList: $((book: Book) => {
+      isToggleOpen.value = false;
+      bookList.readList = bookList.readList.filter(
+        (b: Book) => b.title !== book.title
+      );
+      bookList.books = [...bookList.books, book];
+    }),
+  });
+
+  useContextProvider(ReadListContext, bookStore);
   /**
    * The root of a QwikCity site always start with the <QwikCityProvider> component,
    * immediately followed by the document's <head> and <body>.
    *
    * Don't remove the `<head>` and `<body>` elements.
    */
-
 
   return (
     <QwikCityProvider>
